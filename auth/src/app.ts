75722400e83f6b379fbce4e6fd37e3495c6c55f5
Wayne Links <wayne.links@gmail.com>
2020-11-14 13:05:23 +0200

    build(routes): setup routes controller

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,11 @@
 import 'express-async-errors'
 import express from 'express'
 
+import { BASE_API_V1 } from './config'
+import { routes } from './routes/v1'
+
 const app = express()
+
+app.use(`${BASE_API_V1}`, routes)
 
 export { app }
